21a5986fcb7c347035290ee1cb801bbd033aa4d5
kodlon <kodlon.igorchuchman@gmail.com>
2026-09-17 14:52:01 +0300

    Finished the tutorial

---
+++ Services/PizzaService.cs
@@ -0,0 +1,55 @@
+﻿using PizzaProject.Models;
+
+namespace PizzaProject.Services;
+
+public class PizzaService
+{
+    private static List<Pizza> Pizzas { get;  }
+
+    private static int nextId = 3;
+    
+    static PizzaService() =>
+        Pizzas =
+        [
+            new Pizza
+            {
+                Id = 1,
+                Name = "Pepperoni",
+                IsGlutenFree = false
+            },
+            new Pizza
+            {
+                Id = 2,
+                Name = "Margherita",
+                IsGlutenFree = true
+            }
+        ];
+
+    public static List<Pizza> GetAll() => Pizzas;
+    
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+    
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+    
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if (pizza == null)
+            return;
+
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if (index == -1)
+            return;
+
+        Pizzas[index] = pizza;
+    }
+}
